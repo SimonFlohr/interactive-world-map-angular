cd718e769aae00fb0a81210d4284a871f26dd505
Simon Flohr <sflohr2@wgu.edu>
2024-01-07 15:23:16 -0600

    Create 'map-svg' sub-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorldMapComponent } from './world-map/world-map.component';
+import { MapSvgComponent } from './world-map/map-svg/map-svg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WorldMapComponent
+    WorldMapComponent,
+    MapSvgComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/world-map/map-svg/map-svg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-svg',
+  templateUrl: './map-svg.component.html',
+  styleUrl: './map-svg.component.css'
+})
+export class MapSvgComponent {
+
+}
